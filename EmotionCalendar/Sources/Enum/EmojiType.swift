6d80511b0626a7236269a65f6e8d222f09966477
dlguszoo <lhjivy1024@gmail.com>
2025-08-18 15:13:42 +0900

    [#1] Chore: 폴더 및 주석 정리

--- EmotionCalendar/Sources/Enum/EmojiType.swift
+++ EmotionCalendar/Sources/Enum/EmojiType.swift
@@ -23,24 +23,4 @@ enum EmojiType: CaseIterable {
         case .happy: return "😊"
         }
     }
-
-//    var valence: Double {
-//        switch self {
-//        case .angry:       return -0.9
-//        case .sad:         return -0.5
-//        case .indifferent: return  0.0
-//        case .satisfied:   return  0.5
-//        case .happy:       return  0.9
-//        }
-//    }
-//    
-//    var label: String {
-//        switch self {
-//        case .angry:       return "Angry"
-//        case .sad:         return "Sad"
-//        case .indifferent: return "Indifferent"
-//        case .satisfied:   return "Satisfied"
-//        case .happy:       return "Happy"
-//        }
-//    }
 }
